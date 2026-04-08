a9ce18d9a17f74f1401ae3a2a9b680641ad08d0c
Abduraufdev0788 <turkeynumber063@gmail.com>
2026-04-08 23:42:05 +0500

    bot tayyor

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+BASE_URL = os.getenv("BASE_URL")
--- message/message.py
+++ message/message.py
@@ -1,5 +1,8 @@
 from telegram import Update
 from telegram.ext import CallbackContext, ConversationHandler
+from state import ASK_PHONE
+from config import BASE_URL
+import requests
 
 def get_phone(update: Update, context: CallbackContext):
     contact = update.message.contact
@@ -8,6 +11,16 @@ def get_phone(update: Update, context: CallbackContext):
     user_id = contact.user_id
 
     update.message.reply_text(f"Raqam olindi: {phone}")
+    data = {
+        "telegram_id": user_id,
+        "phone_number": phone,
+        "name": update.effective_user.first_name
+    }
+    response = requests.post(BASE_URL, json=data)
+    if response.status_code == 201:
+        update.message.reply_text("Raqamingiz muvaffaqiyatli saqlandi!")
+    else:
+        update.message.reply_text("Raqamingiz saqlanishda xatolik yuz berdi. Iltimos, qayta urinib ko'ring.")
 
 
     return ConversationHandler.END
